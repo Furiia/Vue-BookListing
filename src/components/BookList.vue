9f6bb4cf86717b72dd211d2e3e6f219f6cf47106
Furiia <furiia@kheshtar.pl>
2021-02-23 21:19:55 +0100

    module 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -7,16 +7,24 @@
       <li>Legacy : Hugo Jackson</li>
       <book-item v-for="book in books" v-bind:book="book"></book-item>
     </ul>
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
   name: 'BookList',
   components: {
-    BookItem
+    BookItem,
+    BookForm
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    }
   },
   data() {
     return {
