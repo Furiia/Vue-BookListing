709600ca1bf0ac774fea470f1d0e577fde05fc5d
Furiia <furiia@kheshtar.pl>
2021-02-23 19:25:20 +0100

    module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <ul>
+      <li>Losing My Religion : Kyell Gold</li>
+      <li>In Wild Lands : Jim Galford</li>
+      <li>Legacy : Hugo Jackson</li>
+      <li v-for="book in books">{{ book.title }} : {{ book.author }}</li>
+    </ul>
   </div>
 </template>
 
@@ -10,6 +16,11 @@ export default {
   data() {
     return {
       title: 'All Books',
+      books: [
+        { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+        { title: 'American Gods', author: 'Neil Gaiman' },
+        { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
+      ]
     };
   },
 };
@@ -18,5 +29,15 @@ export default {
 <style>
 h1, h2 {
   font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
